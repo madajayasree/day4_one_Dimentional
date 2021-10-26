3923b8eb9b78ab7d14fb66fcdf9249801e3d42e4
madajayasree <93071345+madajayasree@users.noreply.github.com>
2021-10-26 22:54:05 +0530

    Add files via upload

---
+++ 1D_array/1D_array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1D_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] sal;
+            Console.WriteLine("Enter Employee count: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            sal = new int[n];
+           
+            for (int i = 0; i < n; i++)
+            {
+                sal[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            int maxSal = sal.Max();
+            Console.WriteLine("maximum salary of emploee : " + maxSal);
+
+            int minSal = sal.Min();
+            Console.WriteLine("minium salary of employye: " + minSal);
+
+            int avgSal = Convert.ToInt32(sal.Average());
+            Console.WriteLine("average salary of employee : " + avgSal);
+
+
+
+        }
+    }
+}
+
